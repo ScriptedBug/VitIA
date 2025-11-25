45c0ef4c4c2e93bcc8a22d934e06c20cd02e0d39
Joan Soler Signes <JSOLSIG@upv.edu.es>
2025-11-25 23:19:53 +0100

    Todo arreglado

--- frontend/lib/pages/auth/login_page.dart
+++ frontend/lib/pages/auth/login_page.dart
@@ -1,29 +1,9 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-<<<<<<< HEAD
-// Quitar dart:convert y http
-// import 'dart:convert';
-// import 'package:http/http.dart' as http;
-// Quitar shared_preferences
-// import 'package:shared_preferences/shared_preferences.dart'; 
-
-// ⬅️ NUEVA IMPORTACIÓN DEL SERVICIO
-import '../../core/services/auth_service.dart'; 
-import '../../core/services/api_config.dart'; // Mantener api_config
-import '../main_layout/home_page.dart';
-=======
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:vinas_mobile/core/services/user_sesion.dart';
 import '../../core/services/api_config.dart';
 import '../main_layout/home_page.dart'; // Importa la nueva ubicación
->>>>>>> bfada624155ab1db2c923d47e78c8c0d67f0c324
-=======
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import '../../core/services/api_config.dart';
-import '../main_layout/home_page.dart'; // Importa la nueva ubicación
->>>>>>> parent of 26f4360 (ubicacion)
 import 'register_page.dart';
 
 class LoginPage extends StatefulWidget {
@@ -53,24 +33,12 @@ class _LoginPageState extends State<LoginPage> {
 
       if (!mounted) return;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text("Inicio de sesión exitoso")),
-      );
-=======
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
         final token = data["access_token"];
 
         UserSession.setToken(token);
         print("Token guardado en sesión: $token");
->>>>>>> bfada624155ab1db2c923d47e78c8c0d67f0c324
-=======
-      if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
-        print("TOKEN: ${data["access_token"]}");
->>>>>>> parent of 26f4360 (ubicacion)
 
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text("Inicio de sesión exitoso")),
